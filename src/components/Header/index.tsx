a949b3ce476fa83e487f5e159b39a90aa706680f
ShokhrukhMakhmudov <msshohruh@gmail.com>
2024-12-21 12:25:41 +0500

    user dropdown and logout functionality

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -42,6 +42,11 @@ export default function Header() {
     checkToken();
   }, []);
 
+  function handleLogout() {
+    localStorage.removeItem("token");
+    window.location.reload();
+  }
+
   return (
     <header className="shadow-lg">
       <div className="container">
@@ -72,7 +77,107 @@ export default function Header() {
               </li>
             </ul>
           </div>
+
           <div className="navbar-end items-center gap-4">
+            {loading ? (
+              <span className="loading loading-spinner loading-lg text-primary" />
+            ) : (
+              <AuthModal
+                content="Kirish"
+                className={`${
+                  user?._id ? "hidden" : ""
+                } btn btn-primary btn-soft`}
+              />
+            )}
+            <div
+              className={`${
+                user?._id ? "" : "hidden"
+              } flex items-center gap-4`}>
+              <div className="indicator">
+                {user?.cart && user.cart.length > 0 && (
+                  <span className="indicator-item badge badge-primary rounded-full">
+                    {user.cart.length}
+                  </span>
+                )}
+                <Link
+                  href="#" // /cart
+                  className="btn btn-primary btn-outline btn-square"
+                  aria-label="Button Indicator">
+                  <span className="icon-[tabler--shopping-bag] size-5" />
+                </Link>
+              </div>
+
+              <div
+                className={`dropdown relative inline-flex rtl:[--placement:bottom-end]`}>
+                <button
+                  id="dropdown-avatar"
+                  type="button"
+                  className="dropdown-toggle btn btn-outline btn-primary flex items-center gap-2 rounded-full"
+                  aria-haspopup="menu"
+                  aria-expanded="false"
+                  aria-label="Dropdown">
+                  {user?.fullName &&
+                    user.fullName.split(" ")[0][0] +
+                      user.fullName.split(" ")[1][0]}
+                  <span className="icon-[tabler--chevron-down] dropdown-open:rotate-180 size-4"></span>
+                </button>
+                <ul
+                  className="dropdown-menu dropdown-open:opacity-100 hidden min-w-60"
+                  role="menu"
+                  aria-orientation="vertical"
+                  aria-labelledby="dropdown-avatar">
+                  <li className="dropdown-header gap-3">
+                    {/* <div className="avatar">
+                      <div className="w-10 rounded-full">
+                        <img
+                          src="https://cdn.flyonui.com/fy-assets/avatar/avatar-3.png"
+                          alt="User Avatar"
+                        />
+                      </div>
+                    </div> */}
+                    <div>
+                      <h6 className="text-base-content/90 text-base font-semibold">
+                        {user?.fullName}
+                      </h6>
+                      <small className="text-base-content/50 text-sm font-normal">
+                        {user?.email}
+                      </small>
+                    </div>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      My Profile
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Settings
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Billing
+                    </a>
+                  </li>
+                  <li>
+                    <button className="dropdown-item" onClick={handleLogout}>
+                      <svg
+                        stroke="currentColor"
+                        fill="currentColor"
+                        strokeWidth="0"
+                        viewBox="0 0 24 24"
+                        height="25px"
+                        width="25px"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5 22C4.44772 22 4 21.5523 4 21V3C4 2.44772 4.44772 2 5 2H19C19.5523 2 20 2.44772 20 3V6H18V4H6V20H18V18H20V21C20 21.5523 19.5523 22 19 22H5ZM18 16V13H11V11H18V8L23 12L18 16Z"></path>
+                      </svg>
+                      <span>Chiqish</span>
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            </div>
+
             <div className="dropdown relative inline-flex md:hidden [--auto-close:inside] [--offset:8] [--placement:bottom-end]">
               <button
                 id="dropdown-default"
@@ -106,79 +211,6 @@ export default function Header() {
                 </li>
               </ul>
             </div>
-            {loading ? (
-              <span className="loading loading-spinner loading-lg text-primary" />
-            ) : user ? (
-              <div className="dropdown relative inline-flex [--auto-close:inside] [--offset:8] [--placement:bottom-end]">
-                <button
-                  id="dropdown-avatar"
-                  type="button"
-                  className="dropdown-toggle btn btn-outline btn-primary flex items-center gap-2 rounded-full"
-                  aria-haspopup="menu"
-                  aria-expanded="false"
-                  aria-label="Dropdown">
-                  <div className="avatar">
-                    <div className="size-6 rounded-full">
-                      <img
-                        src="https://cdn.flyonui.com/fy-assets/avatar/avatar-3.png"
-                        alt="User Avatar"
-                      />
-                    </div>
-                  </div>
-                  {user.fullName.split(" ")[0][0] +
-                    user.fullName.split(" ")[1][0]}
-                </button>
-                <ul
-                  className="dropdown-menu dropdown-open:opacity-100 hidden min-w-60"
-                  role="menu"
-                  aria-orientation="vertical"
-                  aria-labelledby="dropdown-avatar">
-                  <li className="dropdown-header gap-3">
-                    <div className="avatar">
-                      <div className="w-10 rounded-full">
-                        <img
-                          src="https://cdn.flyonui.com/fy-assets/avatar/avatar-3.png"
-                          alt="User Avatar"
-                        />
-                      </div>
-                    </div>
-                    <div>
-                      <h6 className="text-base-content/90 text-base font-semibold">
-                        John Doe
-                      </h6>
-                      <small className="text-base-content/50 text-sm font-normal">
-                        jhon@doe.com
-                      </small>
-                    </div>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      My Profile
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Settings
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      Billing
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="#">
-                      FAQs
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            ) : (
-              <AuthModal
-                content="Kirish"
-                className="btn btn-primary btn-soft"
-              />
-            )}
           </div>
         </nav>
       </div>
