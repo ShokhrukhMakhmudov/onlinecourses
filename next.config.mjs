7ca4767765d477d3d423d259eb5a4d4bff128a32
ShokhrukhMakhmudov <msshohruh@gmail.com>
2025-01-13 15:04:08 +0500

    deleted test video and segments

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(ts|mp4|mp3|mov|avi|webm|mkv|hls|ts)$/,
+      type: "asset/resource", // Указывает Webpack на работу с файлами как с ресурсами
+      generator: {
+        filename: "static/media/[name].[hash][ext]", // Где сохранять файлы в структуре сборки
+      },
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
